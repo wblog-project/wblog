7eb632ec9065c56fc13f5fd290950e61b5ed1f06
REXWind_W <1114864532@qq.com>
2026-08-30 23:10:17 +0800

    fix: run mobile e2e tests in chromium

--- playwright.config.ts
+++ playwright.config.ts
@@ -16,6 +16,6 @@ export default defineConfig({
   },
   projects: [
     { name: 'desktop', use: { ...devices['Desktop Chrome'] } },
-    { name: 'mobile', use: { ...devices['iPhone 13'] } },
+    { name: 'mobile', use: { ...devices['iPhone 13'], browserName: 'chromium' } },
   ],
 });
